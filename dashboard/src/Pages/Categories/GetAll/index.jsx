0d5eaedee6e8517dd6f525c278bf31bc3ee20a4a
mahdialijafari <sedmahdialijafari@gmail.com>
2025-03-15 17:38:48 +0330

    create basic files

---
+++ dashboard/src/Pages/Categories/GetAll/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function GetAllCategories() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ dashboard/src/Pages/Posts/GetAll/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function GetAllPosts() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
